13c0df7d2c1ec61a6d0f3718944f9d8f0a6143c0
NagasaiManda <138184417+NagasaiManda@users.noreply.github.com>
2024-07-15 08:43:40 +0530

    Add files via upload

---
+++ motion/launch/spawn.launch.py
@@ -0,0 +1,38 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+from launch.actions import DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    package_name='motion'
+
+    rsp = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(package_name), 'launch', 'publisher.launch.py'
+        )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+    )
+
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'my_bot'],
+                        output='screen')
+
+
+    
+
+    return LaunchDescription([
+        rsp,
+        gazebo,
+        spawn_entity,
+    ])
